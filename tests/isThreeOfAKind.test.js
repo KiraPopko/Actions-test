a9f5c93606829c5be1fd286ed254db99d9a2a7c0
KiraPopko <kira.popko@gmail.com>
2024-10-18 14:45:24 +0200

    changes

--- tests/isThreeOfAKind.test.js
+++ tests/isThreeOfAKind.test.js
@@ -6,9 +6,14 @@ const suits = '♥♦♣♠';
 
 // Will fail since no code written in Compare Hands yet for the method
 // isThreeOfAKind
-/*test('Test that threeOfAKind returns truthy is three of a kind', () => {
+test('Test that threeOfAKind returns truthy is three of a kind', () => {
   let hand = new Hand('♥7', '♦2', '♣7', '♠3', '♠7');
   expect(CompareHands.isThreeOfAKind(hand)).toBeTruthy();
+});
+
+/*test('Test that threeOfAKind returns truthy is three of a kind', () => {
+  let hand = new Hand('♥7', '♦7', '♣8', '♠3', '♠7');
+  expect(CompareHands.isThreeOfAKind(hand)).toBeFalsy();
 });*/
 
 test('check that isThreeOfAKind returns a higher score for a stronger hand (if two hands but with three of a kind)', () => {
